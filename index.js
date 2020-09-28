08473a02b5625e9bb57b2b19406b474b9480984c
DrParadox05 <rksingh.rks6661@gmail.com>
2020-09-29 00:32:24 +0530

    First Update

---
+++ index.js
@@ -0,0 +1,28 @@
+Vue.component('mycomponent',{
+    template:'<p>This is my component {{ name }} - {{ temprature }} <button v-on:click="changeTemp()">Click Here</button></p> ',
+
+    data:function(){
+        return{
+            name:'John',
+            temprature: 100,
+        }
+    },
+
+    methods:{
+        changeTemp:function(){
+            this.temprature = this.temprature + 10;
+        }
+    }
+})
+
+var app = new Vue({
+    el:'#app',
+
+    data:{
+        value: 100,
+        price: 2000,
+    },
+    methods:{
+
+    }
+});
