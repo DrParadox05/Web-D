04aa1fd522703a9d968e844c6a69a26606bf9b86
DrParadox05 <rksingh.rks6661@gmail.com>
2021-05-19 13:20:38 +0530

    Upto function expressions

---
+++ Node/app.js
@@ -0,0 +1,31 @@
+//Ways to show output using node
+console.log('Hello There');
+
+setTimeout(function(){
+    console.log('Times Up')
+},5000);
+
+//Function Expressions
+// 1- Normally defining a function
+function displayMessage(){
+    console.log('This is a normal functiion');
+}
+displayMessage();
+
+// 2- Using function expression
+var funcExp = function(){
+    console.log('This is function Expression');
+}
+funcExp();
+
+// 3- Creating 2 functions and using result of one in another
+var getSq = function(num){
+    return num*num;
+}
+
+var getDouble = function(n){
+    return 2*n;
+}
+
+console.log(getSq(getDouble(10)));
+console.log(getDouble(getSq(10)));
