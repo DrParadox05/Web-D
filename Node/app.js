d9e92e3b5161513529005fb4daf423432074c1a7
DrParadox05 <rksingh.rks6661@gmail.com>
2021-06-02 22:24:02 +0530

    Readable and Writable stream

--- Node/app.js
+++ Node/app.js
@@ -1,4 +1,4 @@
-/* //Ways to show output using node
+/*//Ways to show output using node
 console.log('Hello There');
 
 setTimeout(function(){
@@ -70,4 +70,25 @@ var fs = require('fs');
 fs.unlink('demo.txt'); //Removes the demo file
 fs.mkdir('demo'); //Creates a direcatory
 fs.rmdir('demo'); //Removse a directory 
-//Removing and creatitng directory can be used with Sync as well according to use */
+//P.S.-Removing and creatitng directory can be used with Sync as well according to use 
+
+//Creating our Server
+var http = require('http');
+var server = http.createServer(function(request,response){
+    response.writeHead(200,{'Content-Type':'text/plain'});
+    response.end('Hello There!!');
+});
+
+server.listen(5000,'127.0.0.1'); */
+
+//Readableand Writable  Stream
+var fs = require('fs');
+var readStream = fs.createReadStream(__dirname + '/demo.txt');
+var writeStream = fs.createWriteStream(__dirname + '/demowrite.txt');
+var chunk_count = 0;
+readStream.on('data',function(d){
+    //console.log('This is the read data'+ d);
+    writeStream.write(d);
+    chunk_count++;
+    console.log(chunk_count);
+});
